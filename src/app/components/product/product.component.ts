2a6f42985d5a09a18bb086e101edcf9ed65adf05
David <dsbpim@gmail.com>
2023-04-24 07:57:38 -0500

    new :  Shpping cart

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input,EventEmitter, Output } from '@angular/core';
 import { Product } from 'src/app/models/product.model';
 @Component({
   selector: 'app-product',
@@ -6,11 +6,16 @@ import { Product } from 'src/app/models/product.model';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent {
-  
+
   @Input() product : Product = {
     id : 0,
     name : '',
     precio : 0,
     image : ''
+  }
+  @Output() addedProduct = new EventEmitter<Product>();
+  onAddToCart = () => {
+    this.addedProduct.emit(this.product);   
+     
   }
 }
